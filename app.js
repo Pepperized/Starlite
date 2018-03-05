12bf9d8b1eb376c97a0d9fcaa00ba1b873df6517
Unknown <bradley.stapleton@tesco.net>
2018-03-05 13:49:15 +0000

    Title logo added

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
 
 
-app.listen(3000, function () {
-    console.log("Listening on port 3000");
+app.listen(8080, function () {
+    console.log("Listening on port 8080");
 });
